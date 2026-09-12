9576c20b441b90b05fa8f9e52e728776088698e7
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2026-09-12 23:57:41 +0330

    working version

--- scripts/common/hiddify_installer.sh
+++ scripts/common/hiddify_installer.sh
@@ -307,7 +307,6 @@ function update_from_github() {
     rm "$file_name"
 
     bash scripts/install.sh --no-gui --no-log
-#    bash scripts/install.sh --no-gui --no-log #temporary fix
 }
 
 function custom_version_installer(){
